3f9eeb14d3a7d15a900e165fc1c5b59aecd5c7e4
yujin-zero <poly30@naver.com>
2025-05-11 17:55:22 +0900

    build: 초기 세팅

---
+++ src/main/java/com/example/javaadv1/JavaAdv1Application.java
@@ -0,0 +1,13 @@
+package com.example.javaadv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAdv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAdv1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaadv1/JavaAdv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaadv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAdv1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
